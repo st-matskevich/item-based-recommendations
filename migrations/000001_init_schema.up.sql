3aaa42e51c8544aa127257a856ced5cd4e80441f
st-matskevich <st.matskevich@gmail.com>
2021-10-17 16:28:43 +0300

    Remove test uid

--- migrations/000001_init_schema.up.sql
+++ migrations/000001_init_schema.up.sql
@@ -3,6 +3,5 @@ CREATE TABLE likes(user_id INT, post_id INT);
 CREATE TABLE users(user_id SERIAL, firebase_uid VARCHAR(32));
 
 --add testing data
-INSERT INTO users(user_id, firebase_uid) VALUES (1, 'nOf1oG74YASI8DOp5bGT7AiGGa33');
 INSERT INTO likes(user_id, post_id) VALUES (1, 1), (1, 3), (1, 5);
 INSERT INTO post_tag(post_id, tag_id) VALUES (1, 1), (1, 2), (2, 1), (2, 2), (3, 1), (3, 3), (4, 1), (4, 5), (5, 1), (5, 4), (6, 6), (6, 2), (7, 7), (7, 8);
