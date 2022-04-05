95f1c2ce6bb61a390d20458d16fd0b82a5a9875f
st-matskevich <st.matskevich@gmail.com>
2022-04-05 17:19:26 +0300

    Move firebase keys to env

--- internal/firebase/firebase.go
+++ internal/firebase/firebase.go
@@ -2,13 +2,14 @@ package firebase
 
 import (
 	"context"
+	"os"
 
 	firebase "firebase.google.com/go"
 	"google.golang.org/api/option"
 )
 
 func OpenFirebaseClient() error {
-	opt := option.WithCredentialsFile("firebase-key.json")
+	opt := option.WithCredentialsJSON([]byte(os.Getenv("FIREBASE_JSON")))
 	app, err := firebase.NewApp(context.Background(), nil, opt)
 	if err != nil {
 		return err
